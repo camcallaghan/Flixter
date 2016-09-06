341fc2a6dd4ba124de300dd192be9d9e5e67f255
Colum O' Callaghan <camcallaghan@gmail.com>
2016-09-06 12:10:08 +0000

    Model adjusted with migration

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,8 +1,7 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
-
   def new
-    @course =Course.new
+    @course = Course.new
   end
 
   def create
